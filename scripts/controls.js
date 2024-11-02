95042bdb67e74c6eb1946f289da71f63aa0aefd5
kayyraa <zindhukayrabasbay@gmail.com>
2024-11-02 21:09:33 +0300

    Auto-update from local project folder

---
+++ scripts/controls.js
@@ -0,0 +1,28 @@
+import * as Grid from "./grid.js";
+
+let ScaleFactor = 1;
+
+document.addEventListener("wheel", (Event) => {
+    if (Event.ctrlKey) {
+        Event.preventDefault();
+
+        ScaleFactor += Event.deltaY < 0 ? 0.25 : -0.25;
+        ScaleFactor = Math.max(0.25, ScaleFactor);
+        Grid.Grid.style.setProperty("--scale-factor", ScaleFactor);
+        Grid.ZoomLabel.innerHTML = `Zoom: ${ScaleFactor.toFixed(2)}x`;
+    }
+}, { passive: false });
+
+Grid.ZoomInButton.onclick = () => {
+    ScaleFactor += 0.25;
+    ScaleFactor = Math.max(0.25, ScaleFactor);
+    Grid.Grid.style.setProperty("--scale-factor", ScaleFactor);
+    Grid.ZoomLabel.innerHTML = `Zoom: ${ScaleFactor.toFixed(2)}x`;
+};
+
+Grid.ZoomOutButton.onclick = () => {
+    ScaleFactor -= 0.25;
+    ScaleFactor = Math.max(0.25, ScaleFactor);
+    Grid.Grid.style.setProperty("--scale-factor", ScaleFactor);
+    Grid.ZoomLabel.innerHTML = `Zoom: ${ScaleFactor.toFixed(2)}x`;
+};
